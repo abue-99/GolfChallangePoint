7bb7be6bf3a8373a48f3c90d20d97c97e4c314f7
abü <abuech@ab-con.de>
2026-03-23 15:47:32 +0100

    upd

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,17 +1,12 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  darkMode: ["class"],
+export default {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
-
-export default config;
+} satisfies Config;

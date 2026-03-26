4e7787cda6c97ba6465676dfbedc65898f9e8265
abü <abuech@ab-con.de>
2026-03-26 01:02:20 +0100

    upd

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header({user, toggleSidebar}:HeaderProps){
  return (
   <header className='flex items-center bg-green-700 text-white h-14 px-4 gap-4'>
     <button onClick={toggleSidebar}><Menu/></button>
-    <Image src='/GolfChallengePoint_Logo100.png' alt='Logo' width={40} height={40} unoptimized/>
+    <Image src='/GolfChallengePoint_Logo40.png' alt='Logo' width={40} height={40} priority unoptimized/>
     <span className='font-bold text-lg'>Golf Challenge Point</span>
     <div className='ml-auto flex items-center gap-2'>
       <span>{user.firstName} {user.lastName}</span>
